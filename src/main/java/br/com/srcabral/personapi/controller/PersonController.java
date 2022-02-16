341030a81055857c36e7d4b983b54ada5dd8bb0d
Sergio Cabral <scastrocabral@gmail.com>
2022-02-15 21:43:45 -0300

    Develops person create operation

--- src/main/java/br/com/srcabral/personapi/controller/PersonController.java
+++ src/main/java/br/com/srcabral/personapi/controller/PersonController.java
@@ -3,27 +3,26 @@ package br.com.srcabral.personapi.controller;
 
 import br.com.srcabral.personapi.dto.MessageResponseDTO;
 import br.com.srcabral.personapi.entity.Person;
-import br.com.srcabral.personapi.repository.PersonRepository;
+import br.com.srcabral.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    private PersonRepository personRepository;
+
+    private PersonService personService;
 
     @Autowired
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
     @PostMapping
-    public MessageResponseDTO createPerson(@RequestBody Person person){
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID " + savedPerson.getId())
-                .build();
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
+        return personService.createPerson(person);
     }
 }
---
+++ src/main/java/br/com/srcabral/personapi/service/PersonService.java
@@ -0,0 +1,25 @@
+package br.com.srcabral.personapi.service;
+
+import br.com.srcabral.personapi.dto.MessageResponseDTO;
+import br.com.srcabral.personapi.entity.Person;
+import br.com.srcabral.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+}
